afb1a00465d520c4dee01f521de30fbbb543f43c
sambalshikhar <shikharsambal3@gmail.com>
2021-02-22 10:59:54 +0000

    class added

---
+++ corrnet.py
@@ -0,0 +1,175 @@
+from data_prep.preprocessing import *
+from dataloaders.loaders import *
+from models.model import *
+from utils.embed_utils import *
+from utils.trainer_utils import *
+from trainers import *
+from trainers.losses import *
+from evaluation import *
+
+from config import config
+import wandb
+
+import numpy as np
+import pandas as pd
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, models, transforms
+import torch.utils.data
+from torchvision.models import resnet50
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+
+
+class corrnet():
+    def __init__(self):
+
+    self.df_train=pd.read_csv(config['train_csv_path'])
+    self.df_test=pd.read_csv(config['test_csv_path'])
+
+    source=config['source_dir']
+
+    self.text_emb_size = config['text_emb_size']
+    self.training_image_array = h5py.File(config['train_hdf5_name'], 'r')
+    self.testing_image_array = h5py.File(config['test_hdf5_name'],'r')
+
+    self.image_emb_size = config['image_emb_size']
+    self.text_emb_size = config['text_emb_size']
+    self.middle_emb_size = config['middle_emb_size']
+    self.lamda = config['lamda']
+
+    if config['loss_function']=='clip':
+        loss_function=clip_loss()
+    elif config['loss_function']=='corr_loss':
+        loss_function=CorrnetCost(lamda)
+    else:
+        loss_function=clip_corrnet_loss(lamda)
+
+    self.get_fasstext_vector = Fasttext(fasttext_model)        
+
+
+    self.corrnet_model = Corrnet_Model(self.image_emb_size,self.text_emb_size,self.middle_emb_size,self.loss_function)
+    if config['load_pretrained']:
+        corrnet_model.load_state_dict(torch.load(os.path.join(config['source_dir'],config['model_name'])))
+
+    resnet_model = Resnet50()
+    cosine_sim = nn.CosineSimilarity(dim=1, eps=1e-6)  
+
+     if torch.cuda.is_available:
+        self.corrnet_model,self.resnet_model,self.cosine_sim= self.corrnet_model.cuda(),self.resnet_model.cuda(),self.cosine_sim.cuda()  
+
+def train():
+
+    train_transformations = transforms.Compose([torchvision.transforms.ToPILImage(mode="RGB"),
+                                                transforms.RandomHorizontalFlip(p=0.5),
+                                                transforms.RandomResizedCrop(224,interpolation=3),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                                ])
+
+    transformations = transforms.Compose([torchvision.transforms.ToPILImage(mode="RGB"),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                        ])
+
+    train_ds = CustomDataset(self.df_train,self.training_image_array,train_transformations,self.get_fasstext_vector,self.text_emb_size)
+    trainLoader = DataLoader(train_ds, shuffle=True, batch_size=config['batch_size'], pin_memory = torch.cuda.is_available())
+    
+    test_ds = CustomDataset(self.df_test,self.testing_image_array,transformations,self.get_fasstext_vector,self.text_emb_size)
+    testLoader = DataLoader(test_ds, shuffle=False, batch_size=config['batch_size'], pin_memory = torch.cuda.is_available())                                       
+
+
+    if config['optimizer']=='sgd':
+        optimizer = optim.SGD(corrnet_model.parameters(), lr=config['lr'])
+    else:        
+        optimizer = optim.Adam(corrnet_model.parameters(), lr=config['lr'])
+
+    decayRate = config['decayRate']
+
+    my_lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=decayRate)
+
+    n_epochs = config['n_epochs']
+    print_freq = config['print_freq']  
+
+    best_test_img2img,best_test_txt2txt,best_test_img2txt = 0,0,0
+    val_r5_acc_img2txt_best,val_r5_acc_txt2img_best=0,0  
+
+    #wandb stuff
+
+    wandb.init(project=config['project_name'],entity=config['username'])
+    wandb.run.name = config['experiment_name']
+    wandb.run.save()
+    wandb.watch(self.corrnet_model)
+
+    #training loop
+    for epoch in range(0, n_epochs + 1):
+        self.corrnet_model,self.resnet_model=train_epoch(self.corrnet_model,self.trainLoader,self.resnet_model,self.cosine_sim,optimizer,epoch,print_freq,wandb)
+        val_r5_acc_img2txt,val_r5_acc_txt2img=test_epoch(self.corrnet_model,testLoader,self.resnet_model,self.cosine_sim,optimizer,epoch,print_freq,wandb)
+
+        if val_r5_acc_img2txt.avg>=val_r5_acc_img2txt_best or val_r5_acc_txt2img.avg>=val_r5_acc_txt2img_best:
+            val_r5_acc_img2txt_best,val_r5_acc_txt2img_best=val_r5_acc_img2txt.avg,val_r5_acc_txt2img.avg
+            if not os.path.isdir(config['source_dir']):
+                os.mkdir(config['source_dir'])
+            torch.save(corrnet_model.state_dict(),os.path.join(config['source_dir'],config['model_name']))
+            print("YIPPEEE MODEL SAVED")
+            print(f"{best_test_img2img} , {best_test_txt2txt} , {best_test_img2txt}")
+
+def evaluate(retrieval_type,random_seed):
+
+    transformations = transforms.Compose([torchvision.transforms.ToPILImage(mode="RGB"),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                        ])
+    test_ds = CustomDataset(self.df_test,self.testing_image_array,transformations,self.get_fasstext_vector,self.text_emb_size)
+    testLoader = DataLoader(test_ds, shuffle=False, batch_size=config['batch_size'], pin_memory = torch.cuda.is_available())
+
+    if not os.path.isdir("./annoy_indices"):
+        common_emb_pool,only_image_emb_pool,only_text_emb_pool=create_emb_pool(corrnet_model,resnet_model,testLoader)
+        create_annoy_index(common_emb_pool,only_image_emb_pool,only_text_emb_pool)
+
+    retreival_df=self.df_test.sample(config['evaluate_n_results'],random_state=random_seed)
+    test_ds_eval = CustomDataset(retreival_df,testing_image_array,transformations,get_fasstext_vector,text_emb_size)
+    retreival_loader = DataLoader(test_ds_eval, shuffle=False, batch_size=1, pin_memory = torch.cuda.is_available())
+    
+    annoy_index= AnnoyIndex(1024, 'angular')  
+
+    if retrieval_type=='txt2img':   
+        annoy_index.load("./annoy_indices/image.ann")
+    elif retrieval_type=='img2txt': 
+        annoy_index.load("./annoy_indices/text.ann")
+    else:
+        annoy_index.load("./annoy_indices/common.ann")
+
+    retreival_indexes=get_retrievals(retreival_loader,retreival_df,annoy_index,corrnet_model,resnet_model,retrieval_type)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def train(self):
+
+
+
+
+
+
+
+
+
+
---
+++ evaluation/__init__.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.hub import load_state_dict_from_url
+from torchvision.models import resnet50
+import torch.nn.functional as F
+import random
+from annoy import AnnoyIndex
+import os
+import matplotlib.pyplot as plt
+import argparse
+
+
+def create_emb_pool(corrnet_model,resnet_model,testLoader):
+    common_emb_pool = []
+    only_image_emb_pool = []
+    only_text_emb_pool = []
+    with torch.no_grad():
+        for (image_emb, text_emb,_) in tqdm(testLoader):
+            if torch.cuda.is_available():
+                image_emb = image_emb.cuda()
+                text_emb  = text_emb.cuda()
+
+            resnet_model.eval()    
+            resnet_out = resnet_model(image_emb)
+            corrnet_model.eval()
+            common_emb, only_image_emb, only_text_emb, _, _, _ = corrnet_model(resnet_out, text_emb)
+
+            only_image_emb, only_text_emb=F.normalize(only_image_emb, p=2, dim=1),F.normalize(only_text_emb, p=2, dim=1)
+
+            common_emb_pool.append(common_emb.detach().cpu().numpy())
+            only_image_emb_pool.append(only_image_emb.cpu().numpy())
+            only_text_emb_pool.append(only_text_emb.cpu().numpy())
+
+    common_emb_pool = np.vstack(common_emb_pool)
+    only_image_emb_pool = np.vstack(only_image_emb_pool)
+    only_text_emb_pool = np.vstack(only_text_emb_pool)
+
+    common_emb_pool.shape, only_image_emb_pool.shape, only_text_emb_pool.shape
+
+    print("Pool created")
+
+    return common_emb_pool,only_image_emb_pool,only_text_emb_pool
+
+def get_retrievals(loader,df,annoy_index,corrnet_model,resnet_model,retrieval_type):
+        text = df.combined_name_and_breadcrumbs.values
+        id = df._id.values
+
+        full_id=df_test._id.values
+        full_text=df_test.combined_name_and_breadcrumbs.values
+
+        for i,(image_emb, text_emb,_) in enumerate(loader):        
+            print("Query")
+            p = np.array(testing_image_array[str(id[i])])
+            plt.title(text[i])
+            plt.imshow(p)
+            plt.show()
+            
+            corrnet_model.eval()
+            resnet_model.eval()
+            with torch.no_grad():
+                if torch.cuda.is_available():
+                    image_emb = image_emb.cuda()
+                    text_emb  = text_emb.cuda()
+                resnet_out = torch.unsqueeze(resnet_model(image_emb),0)
+                common_emb, only_image_emb, only_text_emb, _, _, _ = corrnet_model(resnet_out, text_emb)
+                common_emb,only_image_emb,only_text_emb=F.normalize(common_emb, p=2, dim=0),F.normalize(only_image_emb, p=2, dim=0),F.normalize(only_text_emb, p=2, dim=0)
+
+            common_emb=common_emb.detach().cpu().numpy()
+            only_image_emb=only_image_emb.cpu().numpy()
+            only_text_emb=only_text_emb.cpu().numpy()
+            
+            if retrieval_type=='txt2img':
+
+                query_vector=only_text_emb
+                retrieved_index = annoy_index.get_nns_by_vector(query_vector[0], 11)[1:]
+
+            elif retrieval_type=='img2txt': 
+                query_vector=only_image_emb   
+                retrieved_index = annoy_index.get_nns_by_vector(query_vector[0], 11)[1:]
+
+            else:
+                query_vector=common_emb
+                retrieved_index = annoy_index.get_nns_by_vector(query_vector[0], 10)[1:]
+
+            print('retrieved_index:', retrieved_index)
+
+            for r_ix in retrieved_index:
+                p = np.array(testing_image_array[str(full_id[r_ix])])
+                print("Name:",full_text[r_ix])
+                plt.imshow(p)
+                plt.show()
+                print()  
+            print("******************")
+
+
+def create_annoy_index(common_emb_pool,only_image_emb_pool,only_text_emb_pool):
+
+    annoy_common = AnnoyIndex(1024, 'angular') 
+    annoy_image = AnnoyIndex(1024, 'angular') 
+    annoy_text = AnnoyIndex(1024, 'angular') 
+
+    for ix in tqdm(range(len(common_emb_pool))):
+
+        IT = common_emb_pool[ix]
+        I_ = only_image_emb_pool[ix]
+        _T = only_text_emb_pool[ix]
+
+        annoy_common.add_item(ix, IT)
+        annoy_image.add_item(ix, I_)
+        annoy_text.add_item(ix, _T)
+        
+    annoy_common.build(10) # 10 trees
+    annoy_image.build(10) # 10 trees
+    annoy_text.build(10) # 10 trees
+
+    
+
+    if not os.path.isdir("./annoy_indices"):
+        os.mkdir("./annoy_indices")
+        annoy_common.save('./annoy_indices/common.ann')
+        annoy_image.save('./annoy_indices/image.ann')
+        annoy_text.save('./annoy_indices/text.ann')
+
+    print("Annoy Indices created")    
